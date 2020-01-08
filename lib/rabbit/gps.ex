e95de5fc287af1df3f5f678df3a85662a0807a10
Michael Whalen <michael@whalesalad.com>
2020-01-08 09:59:44 -0800

    Configure the device and grab nav-pvt!

--- lib/rabbit/gps.ex
+++ lib/rabbit/gps.ex
@@ -4,22 +4,50 @@ defmodule Rabbit.GPS do
 
   @i2c_addr 0x42
 
+  def get_version(ref) do
+    data = Packet.encode(Packet.p(:mon, :ver, []))
+    response_size = 160 + 9
+
+    {:ok, response} = I2C.write_read(ref, @i2c_addr, data, response_size)
+
+    cleaned = response
+      |> :binary.bin_to_list
+      |> Enum.drop_while(fn byte -> byte == 0xFF end)
+      |> :binary.list_to_bin
+
+    IO.inspect([:relevant, response, :cleaned, cleaned])
+
+    <<0xB5, 0x62, msg_class, msg_id, msg_length::16-little, payload::binary>> = cleaned
+
+    IO.inspect(%{
+      msg_class: msg_class,
+      msg_id: msg_id,
+      msg_length: msg_length
+    })
+
+    # <<sw_version::bytes-size(30), hw_version::bytes-size(10), rest::binary>> = payload
+
+    payload
+    |> String.split(<<0>>, trim: true)
+  end
+
   def get_port_configuration(ref) do
-    {:ok, <<0xFF, response::binary>>} = I2C.write_read(
-      ref,
-      @i2c_addr,
-      Packet.encode(Packet.p(:cfg, :prt, [0])),
-      29
-    )
+    data = Packet.encode(Packet.p(:cfg, :prt, []))
+    response_size = 20 + 9
 
-    payload_size = 20 * 8
+    {:ok, response} = I2C.write_read(ref, @i2c_addr, data, response_size)
 
-    <<0xB5, 0x62, _class, _id, _length::16-little, payload::bytes-size(20), _cka, _ckb>> = response
+    IO.inspect(%{ response: response })
+
+    <<0xFF, 0xB5, 0x62, _msg_class, _msg_id, _msg_length::16-little, payload::bytes-size(20), _::binary>> = response
 
     <<
       port_id,
       _,
-      tx_ready::bytes-size(2),
+      thresh,
+      pin::5,
+      pol::1,
+      en::0,
       mode::bytes-size(4),
       _::bytes-size(4),
       in_proto_mask::bytes-size(2),
@@ -28,21 +56,183 @@ defmodule Rabbit.GPS do
       _::bytes-size(2)
     >> = payload
 
+    <<_, _, _, slave_addr::7, _::1>> = mode
+
     %{
       port_id: port_id,
-      tx_ready: tx_ready,
-      mode: mode,
+      thresh: thresh,
+      pin: pin,
+      pol: pol,
+      en: en,
+      slave_addr: slave_addr,
       in_proto_mask: in_proto_mask,
       out_proto_mask: out_proto_mask,
       flags: flags
     }
   end
 
-  def foo() do
-    {:ok, ref} = I2C.open("i2c-1")
+  def configure() do
+    get_ref |> configure
+  end
 
+  def configure(ref) do
+    commands = [
+      # GxGGA off
+      Packet.p(:cfg, :msg, [0xF0,0x00,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # GxGLL off
+      Packet.p(:cfg, :msg, [0xF0,0x01,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # GxGSA off
+      Packet.p(:cfg, :msg, [0xF0,0x02,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # GxGSV off
+      Packet.p(:cfg, :msg, [0xF0,0x03,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # GxRMC off
+      Packet.p(:cfg, :msg, [0xF0,0x04,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # GxVTG off
+      Packet.p(:cfg, :msg, [0xF0,0x05,0x00,0x00,0x00,0x00,0x00,0x01]),
+
+      # NAV-PVT ON
+      Packet.p(:cfg, :msg, [0x01,0x07,0x00,0x01,0x00,0x00,0x00,0x00]),
+
+      # NAV-POSLLH ON
+      Packet.p(:cfg, :msg, [0x01,0x02,0x00,0x01,0x00,0x00,0x00,0x00]),
+
+      # Rate 10Hz
+      Packet.p(:cfg, :rate, [0x64,0x00,0x01,0x00,0x01,0x00]),
+    ]
+
+    commands |> Enum.each(fn packet ->
+      I2C.write(ref, @i2c_addr, Packet.encode(packet))
+
+      # Simulating a 38400baud pace (or less),
+      # otherwise commands are not accepted by the device.
+      :timer.sleep(5)
+    end)
+  end
+
+  def get_pvt do
+    get_pvt(get_ref)
+  end
+
+  def get_pvt(ref) do
+    request = Packet.encode(Packet.p(:nav, :pvt, []))
+    response_size = 92 + 9
+    # response_size = 200
+
+    {:ok, response} = I2C.write_read(ref, @i2c_addr, request, response_size)
+
+    cleaned = response
+      |> :binary.bin_to_list
+      |> Enum.drop_while(fn byte -> byte == 0xFF end)
+      |> :binary.list_to_bin
+
+    <<0xB5, 0x62, msg_class, msg_id, msg_length::16-little, payload::bytes-size(92), _::binary>> = cleaned
+
+    IO.inspect(%{
+      cleaned: cleaned,
+      payload: payload,
+      msg_class: msg_class,
+      msg_id: msg_id,
+      msg_length: msg_length,
+      length: byte_size(payload)
+    })
+
+    <<
+      itow::integer-size(32),
+      year::integer-size(16),
+
+      month,
+      day,
+      hour,
+      min,
+      sec,
+
+      # valid,
+      _::4,
+      valid_mag::1,
+      valid_time::1,
+      fully_resolved::1,
+      valid_date::1,
+
+      _time_accuracy::integer-size(32),
+      _nanoseconds::signed-integer-size(32),
+
+      fix_type,
+
+      # Flags, one byte
+      _flags,
+      # carrier_phase_solution::2,
+      # head_vehicle_valid::1,
+      # power_save_mode::3,
+      # differential_corrections_applied::1,
+      # gnss_fix_ok::1,
+
+      _flags2,
+
+      # confirmed_available::1,
+      # confirmed_date::1,
+      # confirmed_time::1,
+
+      satellites_used,
+
+      # longitude::signed-integer-size(32),
+
+      # latitude::signed-integer-size(32),
+
+      x::binary
+    >> = payload
+
+    <<
+      crap::size(184),
+      longitude::signed-integer-size(32),
+      latitude::signed-integer-size(32),
+      _::binary
+    >> = payload
+
+    %{
+      itow: itow,
+      year: year,
+      month: month,
+      day: day,
+      hour: hour,
+      min: min,
+      sec: sec,
+      valid_mag: valid_mag,
+      valid_time: valid_time,
+      fully_resolved: fully_resolved,
+      valid_date: valid_date,
+      # time_accuracy: time_accuracy,
+      # nanoseconds: nanoseconds,
+      fix_type: fix_type,
+      # carrier_phase_solution: carrier_phase_solution,
+      # head_vehicle_valid: head_vehicle_valid,
+      # power_save_mode: power_save_mode,
+      # differential_corrections_applied: differential_corrections_applied,
+      # gnss_fix_ok: gnss_fix_ok,
+
+      # confirmed_available: confirmed_available,
+      # confirmed_date: confirmed_date,
+      # confirmed_time: confirmed_time,
+      satellites_used: satellites_used,
+
+      # 33, 117
+      longitude: longitude / :math.pow(10, 7),
+      latitude: latitude / :math.pow(10, 7),
+    }
+  end
+
+  def get_ref() do
+    {:ok, ref} = I2C.open("i2c-1")
     ref
-      |> get_port_configuration
+  end
+
+  def foo() do
+    get_ref
+      |> get_pvt
       |> IO.inspect
   end
 end
