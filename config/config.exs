ed125fd638d49e681b0ef0010bc470f16079ab0c
Michael Whalen <michael@whalesalad.com>
2020-01-18 13:34:54 -0800

    Use the new fwup config.

--- config/config.exs
+++ config/config.exs
@@ -5,12 +5,15 @@
 # is restricted to this project.
 use Mix.Config
 
-config :rabbit, target: Mix.target()
+config :rabbit,
+  target: Mix.target()
 
 # Customize non-Elixir parts of the firmware. See
 # https://hexdocs.pm/nerves/advanced-configuration.html for details.
 
-config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+config :nerves, :firmware,
+  rootfs_overlay: "rootfs_overlay"
+  #fwup_conf: "config/fwup.conf"
 
 node_name = if Mix.env() != :prod, do: "rabbit"
 
@@ -19,8 +22,10 @@ config :nerves_init_gadget,
   node_name: node_name,
   node_host: :ip,
   ifname: "wlan0",
+  # ifname: "eth0",
   address_method: :dhcp
 
+# eth0: []
 config :nerves_network, :default,
   wlan0: [
     ssid: "whalesalad",
@@ -40,26 +45,8 @@ config :shoehorn,
 # See https://hexdocs.pm/ring_logger/readme.html for more information on
 # configuring ring_logger.
 
-config :logger, backends: [RingLogger]
-
-keys =
-  [
-    Path.join([System.user_home!(), ".ssh", "id_rsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ecdsa.pub"]),
-    Path.join([System.user_home!(), ".ssh", "id_ed25519.pub"])
-  ]
-  |> Enum.filter(&File.exists?/1)
-
-if keys == [],
-  do:
-    Mix.raise("""
-    No SSH public keys found in ~/.ssh. An ssh authorized key is needed to
-    log into the Nerves device and update firmware on it using ssh.
-    See your project's config.exs for this error message.
-    """)
-
-config :nerves_firmware_ssh,
-  authorized_keys: Enum.map(keys, &File.read!/1)
+config :logger,
+  backends: [RingLogger]
 
 if Mix.target() != :host do
   import_config "target.exs"
